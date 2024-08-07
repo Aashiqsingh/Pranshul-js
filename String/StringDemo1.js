0488d05a97765393e6746c5f0e10f670f7e5c340
Aashiqsingh <aashiqsingh56789@gmail.com>
2024-08-07 19:22:29 +0530

    stringMethod

---
+++ String/StringDemo1.js
@@ -0,0 +1,38 @@
+var str = "Royal technosoft pvt litd."
+
+
+
+
+// var x = str.indexOf("b")
+// console.log(x);
+
+
+// var x = str.includes("l t")
+// console.log(x);
+
+
+
+// var x = str.endsWith("d.")
+// var x = str.startsWith("r")
+// console.log(x);
+
+
+// console.log(str);
+// var x = str.length
+// console.log(x);
+
+// var x = str.charAt(2)
+// console.log(x);
+
+
+// var x = str.charCodeAt(33)
+// console.log(x);
+
+// var x = str.codePointAt(33)
+// console.log(x);
+
+// var str2 = " Mr Dhiraj poojara"
+
+// // var x = str.concat(str2)
+// // var x = str + str2
+// console.log(x);
