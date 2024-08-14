7c4382887295830d56ffe954c0c477b81ece6ed2
Aashiqsingh <aashiqsingh56789@gmail.com>
2024-08-14 18:21:23 +0530

    string

---
+++ String/StringDemo2.js
@@ -0,0 +1,44 @@
+var str = "      Shree gurukrupa vidya sankul    ";
+console.log(str);
+console.log(str.length);
+
+
+
+// var x = str.trim()
+// console.log(x);
+// console.log(x.length);
+
+var x = str.trimEnd()
+console.log(x);
+console.log(x.length);
+
+console.log(str.length);
+
+
+
+
+
+
+
+
+// var x = str.indexOf("g")
+// console.log(x);
+
+
+// var x = str.repeat(3)
+// console.log(x);
+
+// var x = str.slice(1)
+// var x = str.slice(1,4)
+// var x = str.slice(-3)
+// var x = str.slice(-4,-1)
+// console.log(x);
+
+
+// console.log(str.toUpperCase());
+// console.log(str.toLocaleLowerCase());
+
+// var x = str.toString()
+// console.log(x);
+
+
