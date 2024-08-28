2219db39e903ac41b714889251616d1738816cf3
Aashiqsingh <aashiqsingh56789@gmail.com>
2024-08-28 18:38:27 +0530

    functions

---
+++ Function/function1.js
@@ -0,0 +1,39 @@
+// without return without arguments
+
+function demo(){
+    console.log("demo function called");
+    
+}
+
+// demo()
+
+// without return with argument
+// var x = parseInt(prompt("Enter a number"))
+function add(a,b){
+    console.log("Addition = ",a+b);
+    
+}
+
+// add(x,5)
+
+// with return without argument
+
+function address(){
+    return "102 , 103 surbhi complex C.G road"
+}
+
+// var x = address()
+// console.log("Addreesss ---- ",x);
+
+// console.log(address());
+
+
+// with return with argument
+
+function mul(x,y){
+    return x*y;
+}
+
+var x = mul(2,6)
+console.log("Multiply = ",x);
+
