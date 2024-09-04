8463fc6f190a34941b6fb19d86f20c590283220b
Aashiqsingh <aashiqsingh56789@gmail.com>
2024-09-04 20:00:17 +0530

    function

--- Function/function4.js
+++ Function/function4.js
@@ -1,8 +1,29 @@
-function strrev(str){
+// function strrev(str){
 
-    return str.split("").reverse().join("")
+//     return str.split("").reverse().join("")
 
+// }
+
+// var x = strrev("pranshul")
+// console.log(x);
+
+// function countOccurrences(str, char) {
+//     return str.split(char).length -1;
+// }
+
+// console.log(countOccurrences("hello world", "l"));
+
+
+function SumOfDigit(num){
+var sum = 0;
+
+    while(num!=0){
+        let rem = num % 10;
+        sum += rem;
+        num = Math.floor(num / 10);
+    }
+    return sum;
 }
 
-var x = strrev("pranshul")
-console.log(x);
+var x = SumOfDigit(456)
+console.log("Sum of all digits...",x);
