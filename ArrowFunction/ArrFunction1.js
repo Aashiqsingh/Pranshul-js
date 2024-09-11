2394c64e59f9289933ea9f68e9c24c5847484051
Aashiqsingh <aashiqsingh56789@gmail.com>
2024-09-11 18:27:08 +0530

    arrow function

---
+++ ArrowFunction/ArrFunction1.js
@@ -0,0 +1,23 @@
+// function add(x,y){
+
+//     console.log("Addition = ",x+y);
+    
+// }
+// add(3,6)
+
+
+const pi = 3.14;
+
+// const demo = ()=>{
+//     console.log("Demo function called...");
+    
+// }
+
+// demo()
+
+const mul = (a,b)=>{
+    return a*b;
+}
+
+var x = mul(3,6)
+console.log("multiply = ",x);
---
+++ ArrowFunction/ArrFunction2.js
@@ -0,0 +1,20 @@
+// const isEven = (x)=>{
+
+
+//     return x%2==0? "Even" : "Odd";
+// }
+
+// var x = isEven(12)
+// console.log(x);
+
+const isEven = (x)=> x%2==0? "Even" : "Odd";
+
+console.log(isEven(12));
+
+
+
+const mul = (x,y)=> x*y;
+
+
+var x = mul(2,4)
+console.log(x);
