71f43e5d87897b5e4cac3eb46fbc2d2a9e18a878
Aashiqsingh <aashiqsingh56789@gmail.com>
2024-10-09 19:05:47 +0530

    array

---
+++ Array/Array1.js
@@ -0,0 +1,59 @@
+// var arr = ["ram","krishna",202,83849893,true]
+
+// console.log(arr);
+
+var users = ["krishna","jutin","priyank","pranshul","suresh","aashish"]
+
+
+// Ending append / add -- extra data
+
+// var x = users.push("dheeraj")
+// console.log(users);
+// console.log(x);
+
+
+// // last index delete
+// var x = users.pop();
+// console.log(users);
+
+// console.log(x);
+
+// starting index : append / add -- extra data
+
+// users.unshift("dheeraj");
+
+// console.log(users);
+
+// users.shift()
+// console.log(users);
+
+
+
+
+
+
+
+
+// for(let i=0;i<users.length;i++){
+//     console.log(users[i].toUpperCase());
+    
+// }
+
+
+
+
+
+// console.log(users);
+
+// console.log(users[0]);
+// console.log(users[1]);
+// console.log(users[2]);
+
+
+
+
+
+
+// console.log(users.length);
+
+
---
+++ Array/Array2.js
@@ -0,0 +1,46 @@
+
+
+
+// var x= users.concat(arr)
+// console.log(x);
+
+// var x = users.indexOf("suresh")
+// console.log(x);
+
+// var x = users.includes("jutin")
+// console.log(x);
+
+// var x = users.join(" ")
+// console.log(x);
+
+
+// var x = users.reverse()
+// console.log(x);
+
+// var x = users.slice(1)
+// var x = users.slice(1,4)
+// var x = users.slice(-4)
+// var x = users.slice(-4 , -2)
+
+
+
+// console.log(x);
+
+var users = ["krishna","jutin","priyank","pranshul","suresh","aashish"]
+
+var arr = [23,34,45,56]
+
+console.log(users);
+
+
+
+// users.splice(3,0,"hanuman")
+// users.splice(1,1,"rahul")
+// users.splice(1,2,"rahul")
+users.splice(1,1,"rahul","kirpekar","tejas")
+
+
+
+console.log(users);
+
+
