9556debc8d023baf189190996a1b7db9144134ad
Aashiqsingh <aashiqsingh56789@gmail.com>
2024-10-16 20:05:21 +0530

    jsonArray

---
+++ JsonArray/JsonArray1.js
@@ -0,0 +1,76 @@
+var users = [
+    {
+        id : 1,
+        name:"pranshul",
+        age:15,
+        email:"pranshul@gmail.com",
+        mobile:'7826397878'
+    },
+    {
+        id:2,
+        name:"rahul",
+        age:29,
+        email:"rahul@gmail.com",
+        mobile:'87263978'
+    },
+    {
+        id:3,
+        name:"rohan",
+        age:27,
+        email:"rohan@gmail.com",
+        mobile:'9826397878'
+    },
+    {
+        id:4,
+        name:"ronali",
+        age:24,
+        email:"sonali@gmail.com",
+        mobile:'8726978'
+    }
+]
+
+// var mobileNum = users.filter((user)=> {
+//     return user.mobile.length < 10
+// }).forEach((name)=>{
+//     console.log(name.name);
+    
+// })
+
+// console.log(mobileNum);
+
+// var findName = users.filter((user)=> user.name.startsWith("r")).map((fname)=> fname.name)
+// console.log(findName);
+
+
+// users = users.filter((user)=>{
+//     return user.age > 25 
+// }).map((user)=>{
+//     return user.name
+// })
+// console.log(users);
+
+// users = users.filter((user)=> user.age > 25).map((u)=> u.name)
+// console.log(users);
+
+
+
+
+// console.log(users);
+// console.log(users[0].name);
+
+// for(let i=0;i<users.length;i++){
+//     console.log(users[i].name);
+    
+// }
+
+
+// users.forEach((user)=>{
+//     console.log(user.name);
+    
+// })
+
+// var userName = users.map((user)=>{
+//     return user.name.toUpperCase()
+// })
+// console.log(userName);
+
