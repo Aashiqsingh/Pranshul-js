4931e7f1e2fc2e8a555a824a62efd9dc2da22696
Aashiqsingh <aashiqsingh56789@gmail.com>
2024-10-23 19:05:14 +0530

    new json array

---
+++ JsonArray/JsonArray2.js
@@ -0,0 +1,87 @@
+var products = [
+    {
+        id:101,
+        name:"Iphone",
+        category:"Mobile",
+        price:100000,
+        color:["red","black","blue","green","yellow"],
+        isAvailable:true,
+        details:{
+            description:"Apple iPhone 13 Pro Max",
+            weight:1500,
+            screenSize:6.1,
+            batteryLife:48,
+        }
+    },
+    {
+        id:102,
+        name:"Samsung Galaxy S21 Ultra",
+        category:"Mobile",
+        price:75000,
+        color:["white","black","blue","green"],
+        isAvailable:false,
+        details:{
+            description:"Samsung Galaxy S21 Ultra 5G",
+            weight:1600,
+            screenSize:6.7,
+            batteryLife:45,
+        }
+
+    },
+    {
+        id:103,
+        name:"Dell laptop",
+        category:"Laptop",
+        price:85000,
+        color:["white","black","blue","aqua"],
+        isAvailable:false,
+        details:{
+            description:"widows with tablets",
+            weight:1400,
+            screenSize:6.4,
+            batteryLife:42,
+        }
+
+    }
+]
+
+// products.forEach((prod)=>{
+//     if(prod.price > 80000){
+//         console.log(prod);
+        
+//     }
+// })
+// console.log(x);
+
+
+// var x = products.filter((prod)=> prod.color.includes("aqua"))
+// console.log(x);
+
+
+// var sum = 0;
+// products.forEach((prod)=>{
+//     sum += prod.price
+// })
+
+// console.log(sum);
+
+// var x = products.reduce((sum,prod)=> sum + prod.price,0)
+// console.log(x);
+
+
+
+
+// console.log(products);
+
+// products.forEach((prod)=>{
+//     console.log(prod.name);
+    
+// })
+
+// products = products.map((prod)=>{
+//     return prod.name
+// })
+
+// console.log(products);
+
+
